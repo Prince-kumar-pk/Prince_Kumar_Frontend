415c90f10885040d006964889e2abf0361cde73f
prince <princekumar92ssm@gmail.com>
2023-04-18 23:16:37 +0530

    modified all the errors in this Assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import List from './components/List';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <List  
+   
+    items={[
+      {text:"Hello This is Prince Kumar"},
+      {text:"Hello This is Adarsh Shukla"},
+      {text:"Hello This is Akash Mishra"},
+      {text:"Hello This is Anshul Singh"},
+      {text:"Hello This is Prince Kumar"},
+      {text:"Hello This is Adarsh Shukla"},
+
+    ]}
+
+   />
     </div>
   );
 }
